906ad5301540bc93760c8f74aa649e5f15237f6e
ARUNKUMAR <arun559703@gmail.com>
2025-05-14 22:03:44 +0530

    Update app.py

--- mcp_agents/app.py
+++ mcp_agents/app.py
@@ -156,4 +156,4 @@ if __name__ == '__main__':
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
     app = loop.run_until_complete(create_app())
-    app.run(debug=True, port=5000)
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
